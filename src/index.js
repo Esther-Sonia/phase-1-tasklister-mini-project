96b1e46238408640826f5225ce0ac6275a6094a8
Esther-Sonia <esthersonia21@gmail.com>
2025-03-13 13:23:22 +0300

    creates form submission handler to prevent empty tasks

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const taskList = document.querySelector("#tasks");
 
   form.addEventListener("submit", handleFormSubmit);
+
+  function handleFormSubmit(event) {
+    event.preventDefault(); 
+
+    const taskText = taskInput.value.trim();
+    if (!taskText) {
+        alert("Fill the task field!");
+        return;
+    }
+
+    addTask(taskText);
+    taskInput.value = ""; 
+}
+
       
 
 
